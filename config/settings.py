81bbae0585910edc558146019c72d088f4187319
S0ltero <AlexsSobko61@gmail.com>
2022-03-06 15:59:01 +0300

    Add `REST_FRAMEWORK` settings to `settings.py`

--- config/settings.py
+++ config/settings.py
@@ -76,6 +76,16 @@ DJOSER = {
         "token": "account.serializers.TokenSerializer"
     }
 }
+
+# RestFramework settings
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10
+}
+
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = join(os.path.dirname(BASE_DIR), "static")
 STATICFILES_DIRS = []
