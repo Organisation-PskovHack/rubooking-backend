b346b5da771ad7bac07df229dccc2d2e8d6fdb28
S0ltero <AlexsSobko61@gmail.com>
2022-03-06 13:09:37 +0300

    Add `ClientSerializer` serializer

--- account/serializers.py
+++ account/serializers.py
@@ -12,3 +12,14 @@ class HotelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Hotel
         fields = "__all__"
+
+
+class ClientSerializer(serializers.ModelSerializer):
+    last_name = serializers.CharField(source="user.last_name")
+    first_name = serializers.CharField(source="user.first_name")
+    middle_name = serializers.CharField(source="user.middle_name")
+    email = serializers.EmailField(source="user.email")
+
+    class Meta:
+        model = Client
+        fields = "__all__"
