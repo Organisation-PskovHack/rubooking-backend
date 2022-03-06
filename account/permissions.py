c3430d721f0b340ccac0e389c04ee1f64d0f071f
S0ltero <AlexsSobko61@gmail.com>
2022-03-06 15:55:54 +0300

    Add `IsHotel` permission

--- account/permissions.py
+++ account/permissions.py
@@ -2,3 +2,13 @@ from django.contrib.auth import get_user_model
 from rest_framework.permissions import BasePermission
 
 User = get_user_model()
+
+
+class IsHotel(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        return user.type == User.Types.HOTEL
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        return user.id == obj.hotel.pk
