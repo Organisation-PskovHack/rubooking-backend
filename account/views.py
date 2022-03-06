486777a64f3ef08c19b2f4dcc7d41ccbcfefff0a
S0ltero <AlexsSobko61@gmail.com>
2022-03-06 15:57:06 +0300

    Add all required imports to `account.views`

--- account/views.py
+++ account/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+from rest_framework import status
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from djoser.permissions import CurrentUserOrAdmin
+
+from .models import (
+    Hotel,
+    Client,
+    HotelNumber,
+    Review,
+    Booking
+)
+from .serializers import (
+    UserSerializer,
+    HotelSerializer,
+    ClientSerializer,
+    HotelNumberSerializer,
+    ReviewSerializer,
+    BookingSerializer
+)
+from .permissions import (
+    IsHotel,
+    IsClient
+)
+
+User = get_user_model()
 
 # Create your views here.
